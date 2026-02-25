bdefdcd2858a4a33ed7bc21c269e2f665398074b
MarViera <58923241+MarViera@users.noreply.github.com>
2026-02-25 14:02:12 -0600

    Create A4.py

---
+++ A4.py
@@ -0,0 +1,18 @@
+def procesar_contactos(lista_contactos):
+    """
+    Recibe una lista de tuplas con la forma:
+    (nombre_completo, telefono, correo)
+    Entonces, lista_contactos es una tupla
+
+    Debe regresar una lista de strings con el formato:
+    "Nombre — Tel: <telefono>"
+    Así que de lista_contactos debes extraer el nombre y el teléfino
+
+    Solo incluir contactos cuyo correo termine en '@gmail.com'.
+    El nombre debe estar limpio (sin espacios extra) y en formato título.
+    pero, lo anterior, solo lo debes hacer si el correo del contacto es @gmail.com
+    """
+    # Escribe tu solución aquí
+    # Debes borrar "pass" y sustituirlo por el código, solo harás el código de la función,
+    # No es necesario que hagas inputs ni llamadas a la función
+    pass
