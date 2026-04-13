53542e0ebebc9464ce32190f78c906ae7374b2cb
AlexFlores8a <alexander.floresch@uanl.edu.mx>
2026-04-13 13:26:12 -0600

    Update E2.py

--- Examen práctica 2oP/E2.py
+++ Examen práctica 2oP/E2.py
@@ -10,3 +10,16 @@ for coord in puntos:
     lista.append(d)
 tupla = tuple(lista)
 print(tupla)
+
+# Ejercicio 2 — Tuplas
+# Calcula la distancia de cada punto al origen
+
+puntos = ((2, 3), (5, -1), (0, 0), (-3, 4))
+lista = []
+
+for coord in puntos:
+    d = (coord[0]**2 + coord[1]**2)**0.5
+    lista.append(d)
+
+tupla_distancias = tuple(lista)
+print(tupla_distancias)
